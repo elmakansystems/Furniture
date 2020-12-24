cb25469ec1cffde9f77b191747058854ffdc762d
elmakansystems <com>
2020-12-24 15:38:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 30.080172038559574, lng: 30.080172038559574 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
